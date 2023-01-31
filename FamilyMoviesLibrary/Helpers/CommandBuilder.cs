c6c91a795ffaedfc10fad440df9f58da66641a97
HappyFox404 <gareks@yandex.ru>
2023-01-31 22:08:52 +0300

    Complete CommandBuilder

--- FamilyMoviesLibrary/Helpers/CommandBuilder.cs
+++ FamilyMoviesLibrary/Helpers/CommandBuilder.cs
@@ -27,7 +27,62 @@ public class CommandBuilder
         }
         else
         {
-            
+            bool isMark = false;
+            List<string> elements = new List<string>();
+            string temp = String.Empty;
+            foreach (var symbol in command.ToCharArray())
+            {
+                if (isMark == false)
+                {
+                    if (symbol == ' ')
+                    {
+                        elements.Add(temp);
+                        temp = String.Empty;
+                    } 
+                    else if (symbol == '"')
+                    {
+                        isMark = true;
+                    }
+                    else
+                    {
+                        temp+=symbol;
+                    }
+                }
+                else
+                {
+                    if (symbol == '"')
+                    {
+                        isMark = false;
+                        elements.Add(temp);
+                        temp = String.Empty;
+                    }
+                    else
+                    {
+                        temp+=symbol;
+                    }
+                }
+            }
+
+            if (String.IsNullOrWhiteSpace(temp) == false)
+            {
+                elements.Add(temp);
+            }
+
+            elements = elements.Where(x => !string.IsNullOrEmpty(x)).ToList();
+
+            if (elements.Any())
+            {
+                Command = elements.First();
+                if (Command.Length > 1)
+                {
+                    Arguments = elements.Skip(1).ToList();
+                }
+                ValidCommand = true;
+            }
+            else
+            {
+                ValidCommand = false;
+            }
         }
     }
 }
